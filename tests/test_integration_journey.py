a85391f96a672ec6e74b29547bd5ecb8fe41bc34
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 13:52:00 +0100

    Only Use MINUTE

--- tests/test_integration_journey.py
+++ tests/test_integration_journey.py
@@ -4,7 +4,6 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 import pytest
 from fastapi.testclient import TestClient
-
 from app.router import app
 
 
