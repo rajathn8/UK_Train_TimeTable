5e7cddf4ab8e855809ee70f79b443a8187637d91
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 10:51:07 +0100

    Added Default Start Time

--- src/app/settings.py
+++ src/app/settings.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_id: str = "11231"
-    app_key: str = "123121"
+    app_id: str = "add_your_app_id_here"
+    app_key: str = "add_your_app_key_here"
     db_url: str = "sqlite:///train_schedule.db"
     env: str = "DEV"
 
