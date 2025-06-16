63f980f2c58a8398bffd67d733f001db0653d7ca
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 18:28:22 +0100

    Fine Tuning Application

--- src/app/health/exceptions.py
+++ src/app/health/exceptions.py
@@ -1,9 +1,12 @@
 """
 Custom exceptions for the health module
 """
+
 from fastapi import HTTPException
 
 
 class HealthCheckException(HTTPException):
     def __init__(self):
-        super().__init__(status_code=500, detail="Raj Service Failure - contact rajath rao")
+        super().__init__(
+            status_code=500, detail="Raj Service Failure - contact rajath rao"
+        )
