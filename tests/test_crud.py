dd7eb04cb42266e23e4d78e57cbef1b0a20b2980
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 13:42:45 +0100

    Fixed Application Core Logic

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -48,6 +48,7 @@ def test_post_timetable_entry_new(db):
     assert entry.aimed_arrival_time.microsecond == 0
 
 
+
 def test_post_timetable_entry_duplicate(db):
     """Test post_timetable_entry for duplicate entry."""
     logger.info("Testing post_timetable_entry for duplicate entry.")
@@ -69,6 +70,7 @@ def test_post_timetable_entry_duplicate(db):
     assert entry.aimed_departure_time.microsecond == 0
     assert entry.aimed_arrival_time.second == 0
     assert entry.aimed_arrival_time.microsecond == 0
+
 
 
 def test_get_timetable_entries(db):
