090eb44707ac7fdd648c919ed82f0dfc3ce969ca
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 09:17:14 +0100

    Pipeline Fix

--- src/app/settings.py
+++ src/app/settings.py
@@ -3,18 +3,15 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_id: str = os.getenv("app_id", "643703ab")
-    app_key: str = os.getenv("app_key", "4e8e9ed71a593771d1bf5c4cd0e70274")
-    db_url: str = os.getenv("DB_URL", "sqlite:///train_schedule.db")
-    env: str = os.getenv("APP_ENV", "DEV")
+    app_id: str
+    app_key: str
+    db_url: str = "sqlite:///train_schedule.db"
+    env: str = "DEV"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = True
 
 
 settings = Settings()
-
-
-class Secrets(BaseSettings):
-    app_id: str = os.getenv("app_id", "643703ab")
-    app_key: str = os.getenv("app_key", "4e8e9ed71a593771d1bf5c4cd0e70274")
-
-
-secrets = Secrets()
