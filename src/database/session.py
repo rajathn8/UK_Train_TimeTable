7392cdc55c35c29d0aaee2b06291b2ec9db72825
Rajath Rao Web_soltZ <rajathn8@gmail.com>
2025-06-16 08:36:28 +0100

    Added DB Session Handler

---
+++ src/database/session.py
@@ -0,0 +1,23 @@
+import sqlalchemy
+import contextlib
+from app.settings import settings
+
+@contextlib.contextmanager
+def get_engine():
+    engine = sqlalchemy.create_engine(settings.db_url)
+    try:
+        yield engine
+    finally:
+        engine.dispose(close=True)
+
+
+def get_db():
+    with get_engine() as engine:
+        SessionLocal = sqlalchemy.orm.sessionmaker(
+            autocommit=False, autoflush=False, bind=engine
+        )
+        db = SessionLocal()
+        try:
+            yield db
+        finally:
+            db.close()
